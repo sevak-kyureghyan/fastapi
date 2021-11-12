b6d21dd623dcd06282212731a71ad223774b597c
Sevak <sevak@tcf.team>
2021-11-12 18:21:29 +0400

    'Updated'

--- app/main.py
+++ app/main.py
@@ -27,4 +27,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Successfully deployed from CI?CD pipeline"}
+    return {"message": "Successfully deployed from CI/CD pipeline"}
